8653f8544187cedee14916b4cbf6d84c0bab6ae6
GmhLovEDM <gmhlovedm@gmail.com>
2025-11-25 15:37:08 +0800

    project init

---
+++ components/SeeButton/index.vue
@@ -0,0 +1,142 @@
+<template>
+	<!-- #ifdef H5 -->
+	<view :id="seeButtonId" class="see-button" :class="itemClass" @click="onTouchstart($event)">
+	<!-- #endif -->
+	<!-- #ifndef H5 -->
+	<view :id="seeButtonId" class="see-button" :class="itemClass" @tap="onTap" @touchstart="onTouchstart($event)">
+	<!-- #endif -->
+		<slot></slot>
+		<view
+			class="see-button-ripple"
+			:class="{ 'active': active }"
+			:style="{
+				top: rippleTop + 'px',
+				left: rippleLeft + 'px',
+				width: field.finalWidth + 'px',
+				height: field.finalWidth + 'px',
+				'background-color': rippleColor
+			}"
+		></view>
+	</view>
+</template>
+
+<script lang="ts" setup>
+import { ref, nextTick, getCurrentInstance } from 'vue'
+
+let globalId = 0
+
+/** ---------- props ---------- */
+const props = withDefaults(
+  defineProps<{
+    rippleColor?: string
+    itemClass?: string
+  }>(),
+  {
+    rippleColor: 'rgba(0, 0, 0, .15)',
+    itemClass: ''
+  }
+)
+
+/** ---------- emits ---------- */
+const emit = defineEmits<{
+  (e: 'onTap'): void
+}>()
+
+/** ---------- state ---------- */
+globalId++
+const rippleTop = ref(0)
+const rippleLeft = ref(0)
+const active = ref(false)
+const field = ref<any>({})
+const seeButtonId = 'seeButton_' + globalId
+
+/** 获取当前实例，用于 createSelectorQuery().in(this) */
+const instance = getCurrentInstance()
+
+/** ---------- methods ---------- */
+function onTap() {
+  emit('onTap')
+}
+
+function onTouchstart(e: any) {
+  active.value = false
+  nextTick(() => {
+    activeWaves(e)
+  })
+}
+
+function activeWaves(e: any) {
+  getClientRect().then((data: any) => {
+    if (!data?.height) return
+
+    data.finalWidth = data.height > data.width ? data.height : data.width
+    if (!data.finalWidth) return
+
+    field.value = data
+
+    let touchesX: number
+    let touchesY: number
+
+    // #ifdef MP-BAIDU
+    touchesX = e.changedTouches[0].clientX
+    touchesY = e.changedTouches[0].clientY
+    // #endif
+
+    // #ifdef MP-ALIPAY
+    touchesX = e.detail.clientX
+    touchesY = e.detail.clientY
+    // #endif
+
+    // #ifndef MP-BAIDU || MP-ALIPAY
+    touchesX = e.touches[0].clientX
+    touchesY = e.touches[0].clientY
+    // #endif
+
+    rippleTop.value = touchesY - data.top - data.finalWidth / 2
+    rippleLeft.value = touchesX - data.left - data.finalWidth / 2
+
+    nextTick(() => {
+      active.value = true
+    })
+  })
+}
+
+function getClientRect() {
+  return new Promise(resolve => {
+    const query = uni.createSelectorQuery().in(instance)
+    const id = '#' + seeButtonId
+
+    query
+      .select(id)
+      .boundingClientRect(data => {
+        resolve(data)
+      })
+      .exec()
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.see-button {
+	position: relative;
+	overflow: hidden;
+	
+	.see-button-ripple {
+		position: absolute;
+		border-radius: 100%;
+		background-clip: padding-box;
+		pointer-events: none;
+		user-select: none;
+		transform: scale(0);
+		opacity: 1;
+	}
+	
+	.active {
+		opacity: 0;
+		transform: scale(2);
+		transition: opacity 1.2s ease-out, transform 0.5s ease-out;
+	}
+}
+
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="list" v-for="(item, index) in list" :key="index" @tap="clickItem(item)">
+			<text>{{ item.title }}</text>
+		</view>
+	</view>
+</template>
+
+<script lang="ts" setup>
+	import { ref } from 'vue'
+	
+	const list = ref([
+		{
+			title: '按钮',
+			url: '/pages/seeButton/index'
+		}
+	])
+	
+	const clickItem = (item) => uni.navigateTo({
+		url: item.url
+	})
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 50rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+	
+	.list{
+		width: 100%;
+		padding: 24rpx 48rpx;
+		box-sizing: border-box;
+		border: 1px #f1f1f1 solid;
+	}
+</style>
