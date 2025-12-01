656a54aaca2f3d9dd5842119d1ed7b66457a3346
GmhLovEDM <gmhlovedm@gmail.com>
2025-12-01 15:30:19 +0800

    更新了SeeButton命名

--- pages/seeButton/index.vue
+++ pages/seeButton/index.vue
@@ -2,54 +2,54 @@
 	<view class="container">
 		<text class="title">按钮类型</text>
 		<view class="content">
-			<SeeButton title="默认按钮" />
-			<SeeButton title="主要按钮" type="primary" />
-			<SeeButton title="失败按钮" type="error" />
-			<SeeButton title="警告按钮" type="warning" />
-			<SeeButton title="成功按钮" type="success" />
+			<see-button title="默认按钮" />
+			<see-button title="主要按钮" type="primary" />
+			<see-button title="失败按钮" type="error" />
+			<see-button title="警告按钮" type="warning" />
+			<see-button title="成功按钮" type="success" />
 		</view>
 		<text class="title">镂空按钮</text>
 		<view class="content">
-			<SeeButton isHollow title="默认按钮" />
-			<SeeButton isHollow title="主要按钮" type="primary" />
-			<SeeButton isHollow title="失败按钮" type="error" />
-			<SeeButton isHollow title="警告按钮" type="warning" />
-			<SeeButton isHollow title="成功按钮" type="success" />
+			<see-button isHollow title="默认按钮" />
+			<see-button isHollow title="主要按钮" type="primary" />
+			<see-button isHollow title="失败按钮" type="error" />
+			<see-button isHollow title="警告按钮" type="warning" />
+			<see-button isHollow title="成功按钮" type="success" />
 		</view>
 		<text class="title">禁用按钮</text>
 		<view class="content">
-			<SeeButton isDisabled title="默认按钮" />
-			<SeeButton isDisabled title="主要按钮" type="primary" />
-			<SeeButton isDisabled title="失败按钮" type="error" />
-			<SeeButton isDisabled title="警告按钮" type="warning" />
-			<SeeButton isDisabled title="成功按钮" type="success" />
+			<see-button isDisabled title="默认按钮" />
+			<see-button isDisabled title="主要按钮" type="primary" />
+			<see-button isDisabled title="失败按钮" type="error" />
+			<see-button isDisabled title="警告按钮" type="warning" />
+			<see-button isDisabled title="成功按钮" type="success" />
 		</view>
 		<text class="title">按钮大小</text>
 		<view class="content">
-			<SeeButton size="normal" title="默认大小" />
-			<SeeButton size="small" title="小型尺寸" />
-			<SeeButton size="mini" title="最小尺寸" />
-			<SeeButton size="large" title="超大尺寸" />
+			<see-button size="normal" title="默认大小" />
+			<see-button size="small" title="小型尺寸" />
+			<see-button size="mini" title="最小尺寸" />
+			<see-button size="large" title="超大尺寸" />
 		</view>
 		<text class="title">水波动画</text>
 		<view class="content">
-			<SeeButton title="默认动画" isRipple />
-			<SeeButton type="primary" size="normal" title="按钮颜色" isRipple />
-			<SeeButton size="normal" title="动画颜色" isRipple ripple-color="blue" />
-			<SeeButton size="large" title="动画时长(10000ms)" isRipple :ripple-time="10000" ripple-color="red" :mask-time="20000" />
+			<see-button title="默认动画" isRipple />
+			<see-button type="primary" size="normal" title="按钮颜色" isRipple />
+			<see-button size="normal" title="动画颜色" isRipple ripple-color="blue" />
+			<see-button size="large" title="动画时长(10000ms)" isRipple :ripple-time="10000" ripple-color="red" :mask-time="20000" />
 		</view>
 		<text class="title">自定义</text>
 		<view class="content">
-			<SeeButton :customStyle="customStyle" title="自定义宽高" />
-			<SeeButton :radius="24" title="圆角按钮" />
-			<SeeButton size="large" title="渐变色按钮" textColor="#FFFFFF" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))" isRipple />
-			<SeeButton title="自定义颜色" color="#8A4D35" textColor="#FFFFFF" isRipple />
-			<SeeButton title="传入一个view">
+			<see-button :customStyle="customStyle" title="自定义宽高" />
+			<see-button :radius="24" title="圆角按钮" />
+			<see-button size="large" title="渐变色按钮" textColor="#FFFFFF" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))" isRipple />
+			<see-button title="自定义颜色" color="#8A4D35" textColor="#FFFFFF" isRipple />
+			<see-button title="传入一个view">
 				<view style="width: 10px; height: 10px; border: 1px red solid" />
-			</SeeButton>
-			<SeeButton size="large">
+			</see-button>
+			<see-button size="large">
 				<text>标题以slot形式</text>
-			</SeeButton>
+			</see-button>
 		</view>
 	</view>
 </template>
